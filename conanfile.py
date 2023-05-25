6490b44893e55de714c566854ba0c7c97b9dc5ec
Ulrich Eck <ulrich.eck@tum.de>
2023-05-25 15:32:48 +0200

    upgraded to conan v2

--- conanfile.py
+++ conanfile.py
@@ -1,29 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanException
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.scm import Git
+from conan.tools.files import load, update_conandata, copy, replace_in_file, collect_libs, get
+from conans.errors import ConanInvalidConfiguration
 import os
 
-def sort_libs(correct_order, libs, lib_suffix='', reverse_result=False):
-    # Add suffix for correct string matching
-    correct_order[:] = [s.__add__(lib_suffix) for s in correct_order]
-
-    result = []
-    for expectedLib in correct_order:
-        for lib in libs:
-            if expectedLib == lib:
-                result.append(lib)
-
-    if reverse_result:
-        # Linking happens in reversed order
-        result.reverse()
-
-    return result
 
 class LibnameConan(ConanFile):
     name = "yuv"
     version = "1749"
+
     description =   "a C++ libary to parse yuv files."
     # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
     topics = ("conan", "corrad", "magnum", "filesystem", "console", "environment", "os")
@@ -31,81 +20,92 @@ class LibnameConan(ConanFile):
     homepage = "https://github.com/ulricheck/libyuv"
     author = "ulrich eck"
     license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+
     exports = ["CMakeLists.txt", "LICENSE.md"]
     # exports_sources = ["CMakeLists.txt", "patches/*"]
-    generators = "cmake"
-    short_paths = True  # Some folders go out of the 260 chars path length scope (windows)
 
     # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False], 
         "fPIC": [True, False],
+        "with_jpeg": [False, "libjpeg", "libjpeg-turbo"],
     }
     default_options = {
         "shared": False, 
         "fPIC": True,
+        "with_jpeg": "libjpeg",
     }
 
-    # Custom attributes for Bincrafters recipe conventions
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
 
-    requires = (
-        "libjpeg-turbo/2.0.5",
-    )
-
-    # temporary until release fixes interconnect issues on windows/release builds
-    scm = {
-        "type": "git",
-        "subfolder": _source_subfolder,
-        "url": "https://github.com/ulricheck/libyuv.git",
-        "revision": "v%s" % (version),
-        "submodule": "recursive",
-    }
+    def requirements(self):
+        if self.options.with_jpeg == "libjpeg":
+            self.requires("libjpeg/9d")
+        elif self.options.with_jpeg == "libjpeg-turbo":
+            self.requires("libjpeg-turbo/2.0.5")
 
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
+        # if str(self.options.with_jpeg) == "libjpeg-turbo":
+        #     raise ConanInvalidConfiguration(
+        #         "libjpeg-turbo is an invalid option right now, as it is not supported by the cmake script.")
 
     def configure(self):
-        if self.options.shared:
+        if self.options.shared and self.options.with_jpeg == "libjpeg-turbo":
             self.options["libjpeg-turbo"].shared = True
 
-    def _configure_cmake(self):
 
-        cmake = CMake(self)
+    def export(self):
+        update_conandata(self, {"sources": {
+            "commit": "v{}".format(self.version),
+            "url": "https://github.com/ulricheck/libyuv.git"
+        }})
+
+    def source(self):
+        git = Git(self)
+        sources = self.conan_data["sources"]
+        git.clone(url=sources["url"], target=self.source_folder, args=["--recursive", ])
+        git.checkout(commit=sources["commit"])
+
+    def generate(self):
+        tc = CMakeToolchain(self)
 
         def add_cmake_option(option, value):
             var_name = "{}".format(option).upper()
             value_str = "{}".format(value)
-            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str 
-            cmake.definitions[var_name] = var_value
+            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str
+            tc.variables[var_name] = var_value
 
         for option, value in self.options.items():
             add_cmake_option(option, value)
+        tc.generate()
 
-        cmake.configure(build_folder=self._build_subfolder)
+        deps = CMakeDeps(self)
+        deps.generate()
 
-        return cmake
+
+    def layout(self):
+        cmake_layout(self, src_folder="source_folder")
 
     def build(self):
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
-            """  target_link_libraries( yuvconvert ${JPEG_LIBRARY} )""",
-            """  target_link_libraries( ${ly_lib_shared} ${JPEG_LIBRARY} )
-  target_link_libraries( yuvconvert ${JPEG_LIBRARY} )"""
-            )
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
-            """INSTALL ( PROGRAMS ${CMAKE_BINARY_DIR}/yuvconvert\t\t\tDESTINATION bin )""",
-            """# INSTALL ( PROGRAMS ${CMAKE_BINARY_DIR}/yuvconvert\t\t\tDESTINATION bin )""",
-            )
-        cmake = self._configure_cmake()
+  #       tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+  #           """  target_link_libraries( yuvconvert ${JPEG_LIBRARY} )""",
+  #           """  target_link_libraries( ${ly_lib_shared} ${JPEG_LIBRARY} )
+  # target_link_libraries( yuvconvert ${JPEG_LIBRARY} )"""
+  #           )
+  #       tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+  #           """INSTALL ( PROGRAMS ${CMAKE_BINARY_DIR}/yuvconvert\t\t\tDESTINATION bin )""",
+  #           """# INSTALL ( PROGRAMS ${CMAKE_BINARY_DIR}/yuvconvert\t\t\tDESTINATION bin )""",
+  #           )
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = collect_libs(self)
+
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,13 +2,29 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.build import can_run
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -16,5 +32,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        bin_path = os.path.join("bin", "test_package")
-        self.run(bin_path, run_environment=True)
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(cmd, env="conanrun")
